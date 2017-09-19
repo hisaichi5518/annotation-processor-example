7b41cc527895434d3272ed3c7b7b5e52c6ae034a
hisaichi5518 <hisaichi5518@gmail.com>
2017-09-19 16:44:31 +0900

    Add process method to KonohanaProcessor

--- processor/src/main/java/com/github/hisaichi5518/konohana/processor/KonohanaProcessor.java
+++ processor/src/main/java/com/github/hisaichi5518/konohana/processor/KonohanaProcessor.java
@@ -1,4 +1,28 @@
 package com.github.hisaichi5518.konohana.processor;
 
-public class KonohanaProcessor {
+import com.google.auto.service.AutoService;
+
+import java.util.Set;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Processor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.annotation.processing.SupportedSourceVersion;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.TypeElement;
+import javax.tools.Diagnostic;
+
+@AutoService(Processor.class)
+@SupportedAnnotationTypes({"com.github.hisaichi5518.konohana.annotation.Key"})
+@SupportedSourceVersion(SourceVersion.RELEASE_8)
+public class KonohanaProcessor extends AbstractProcessor {
+    @Override
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+
+        processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR, "yuriko");
+
+        return false;
+    }
 }
+
