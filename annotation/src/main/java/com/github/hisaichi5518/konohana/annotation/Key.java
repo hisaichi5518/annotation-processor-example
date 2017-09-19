13eef3ebc23c572f4a1e04799aa09d1689239b08
hisaichi5518 <hisaichi5518@gmail.com>
2017-09-19 16:57:20 +0900

    Add Key annotation

--- annotation/src/main/java/com/github/hisaichi5518/konohana/annotation/Key.java
+++ annotation/src/main/java/com/github/hisaichi5518/konohana/annotation/Key.java
@@ -1,4 +1,8 @@
 package com.github.hisaichi5518.konohana.annotation;
 
-public class Key {
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+public @interface Key {
 }
